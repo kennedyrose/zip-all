0358bbda3a7d5cacb8eda76653fb41b1a00463db
Kennedy Rose <kennedy@kennedyrose.com>
2017-05-22 18:59:41 -0500

    Options

--- bin.js
+++ bin.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+const zipAll = require('./index')
+const meow = require('meow')
+const cli = meow(`
+	Usage
+	  $ zip-all
+	Options
+	  -p, --prefix   Zips all directories that start with...
+`, {
+	alias: {
+		p: 'prefix'
+	}
+})
+zipAll(cli.flags)
--- index.js
+++ index.js
@@ -5,11 +5,55 @@ const path = require('path')
 
 
 
-function getDirectories (srcpath) {
-	return fs.readdirSync(srcpath)
-		.filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
+function getDirectories(srcpath, options){
+	return new Promise((resolve, reject) => {
+		fs.readdir(srcpath, (err, files) => {
+			const arr = []
+			for(let i = files.length; i--;){
+				const file = files[i]
+				if(fs.lstatSync(path.join(srcpath, file)).isDirectory() && (!options.prefix || files[i].indexOf(options.prefix) === 0)){
+					arr.push(file)
+				}
+			}
+			resolve(arr)
+		})
+	})
 }
 
-function zipDir(dir){
-	exec()
+function zipDirectories(dirs){
+	return new Promise((resolve, reject) => {
+		const promises = []
+		for(let i = dirs.length; i--;){
+			promises.push(zipDirectory(dirs[i]))
+		}
+		Promise.all(promises)
+			.then(resolve)
+			.catch(reject)
+	})
+}
+
+function zipDirectory(dir){
+	return new Promise((resolve, reject) => {
+		let name = dir.split('/')
+		for(let i = name.length; i--;){
+			if(name[i]){
+				name = name[i]
+				break
+			}
+		}
+		exec(`zip -r ${name} ${dir}`, (err, stdout, stderr) => {
+			console.log(`Zipped ${name}`)
+			if(err) return reject(err)
+			if(stdout) console.log(stdout)
+			if(stderr) console.log(stderr)
+			resolve()
+		})
+	})
+}
+
+module.exports = options => {
+	getDirectories(process.cwd(), options)
+		.then(zipDirectories)
+		.then(() => console.log('Done!'))
+		.catch(console.error)
 }
